78b3b43d2a148daeedca7d48d45add065e38a3d2
Marc Baetica <marc.baetica@gmail.com>
2021-12-18 21:39:52 -0500

    Fix ssh window closing prematurely.

--- ec2/terraform/execute_tf_commands.py
+++ ec2/terraform/execute_tf_commands.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 from pprintpp import pprint
-
+from time import sleep
 
 def create_environment_if_not_exists_and_open_ssh():
     # subprocess.run('terraform init'
@@ -9,6 +9,8 @@ def create_environment_if_not_exists_and_open_ssh():
     # subprocess.run('terraform plan')
     subprocess.run('terraform apply -auto-approve')
 
+    print('Waiting for a few seconds to allow SSH service to start.')
+    sleep(10)
     print('Opening SSH window.')
     ec2_ip = subprocess.check_output('terraform output -json ec2_public_ip').decode('utf-8').replace('"', '')
     command = f'ssh -i "Automation-Ohio.pem" -o StrictHostKeyChecking=no ubuntu@{ec2_ip}'
@@ -19,5 +21,5 @@ def destroy_environment():
     subprocess.run('terraform destroy -auto-approve')
 
 
+destroy_environment()  # Destroy if already exists.
 create_environment_if_not_exists_and_open_ssh()
-# destroy_environment()
